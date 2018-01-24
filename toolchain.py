a8e7e88d0778dc17670393954817880febd1e1a7
Mattias Jansson <mattias@rampantpixels.com>
2018-01-24 21:14:15 +0100

    bump to latest scripts

--- toolchain.py
+++ toolchain.py
@@ -73,7 +73,10 @@ class Toolchain(object):
     else:
       self.libprefix = 'lib'
       self.staticlibext = '.a'
-      self.dynamiclibext = '.so'
+      if target.is_macos() or target.is_ios():
+        self.dynamiclibext = '.dylib'
+      else:
+        self.dynamiclibext = '.so'
       self.binprefix = ''
       self.binext = ''
 
